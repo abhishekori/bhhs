2c752ffe601f9609566d3b692c6ebf9b32bf72b3
abhishek <abhikori1994@gmail.com>
2017-05-04 14:14:23 +0530

    basic template done

--- js/bh-main.js
+++ js/bh-main.js
@@ -1,3 +1,9 @@
 /**
  * Created by abhishek on 2/5/17.
  */
+$('#navbar-toggle').click(function () {
+
+
+    $('.navbar-collapse').css('margin-top','3.1%');
+
+})
--- js/bh-maps.js
+++ js/bh-maps.js
@@ -1,22 +1,12 @@
-var myCenter=new google.maps.LatLng(13.13548,75.641826);
-var marker;
 
-function initialize()
-{
-    var mapProp = {
-        center:myCenter,
-        zoom:12,
-        mapTypeId:google.maps.MapTypeId.ROADMAP
-    };
-
-    var map=new google.maps.Map(document.getElementById("MyMap"),mapProp);
-
-    var marker=new google.maps.Marker({
-        position:myCenter,
-        animation:google.maps.Animation.BOUNCE
+function myMap() {
+    var mapCanvas = document.getElementById("map");
+    var myCenter = new google.maps.LatLng(13.363284, 75.650336);
+    var mapOptions = {center: myCenter, zoom: 16};
+    var map = new google.maps.Map(mapCanvas,mapOptions);
+    var marker = new google.maps.Marker({
+        position: myCenter,
+        animation: google.maps.Animation.BOUNCE
     });
-
     marker.setMap(map);
 }
-
-google.maps.event.addDomListener(window, 'load', initialize);
